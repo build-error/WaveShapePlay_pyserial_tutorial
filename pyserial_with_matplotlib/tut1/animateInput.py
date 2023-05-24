758fbb33c71980dfafe5bd97f39a2c70e6401d01
Aryan Singh <aryan7109singh@gmail.com>
2023-05-25 02:10:48 +0530

    Update animateInput.py

--- pyserial_with_matplotlib/tut1/animateInput.py
+++ pyserial_with_matplotlib/tut1/animateInput.py
@@ -2,42 +2,52 @@ import time
 import serial
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import statistics as stats
 
 def getValues(serCom, input, encodeFormat):
     serCom.write(str(input).encode(encodeFormat))
     data =  serCom.readline().decode(encodeFormat).rstrip()
     return data
 
-def animateX(i, dataListX, ser, userInput, encoding):
+def animate(i, dataListX, dataListY, ser, userInput, encoding):
     dataString = getValues(ser, userInput, encoding)
-    
+    print(dataString)
+
     try:
         dataInt = [int(x) for x in dataString.split(',')]
         dataListX.append(dataInt[0])
+        dataListY.append(dataInt[1])
 
     except:
         pass
 
     dataListX = dataListX[-50:]
+    dataListY = dataListY[-50:]
 
     ax.clear()
     ax.plot(dataListX)
-
-    ax.set_ylim([0, 1023])
+    ax.set_ylim([0, 1050])
     ax.set_title("Xval data")
     ax.set_ylabel("Value")
+
+    ay.clear()
+    ay.plot(dataListY)
+    ay.set_ylim([0, 1050])
+    ay.set_title("Yval data")
+    ay.set_ylabel("Value")
+
 
 if __name__ == '__main__':
     ser = serial.Serial('COM5', baudrate=115200, timeout=1)
 
     time.sleep(3)
 
-    dataListX = []
+    dataListX, dataListY = [], []
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
+    # fig, (ax, ay) = plt.subplots(2,1)
+    fig = plt.figure(tight_layout=True)
+    (ax, ay) = fig.subplots(2,1)
 
-    ani = animation.FuncAnimation(fig, animateX, frames=100, fargs=(dataListX, ser, 'y', 'ascii'), interval=10)
+    aniX = animation.FuncAnimation(fig, animate, frames=100, fargs=(dataListX, dataListY, ser, 'y', 'ascii'), interval=10)
+
     plt.show()
     ser.close()
